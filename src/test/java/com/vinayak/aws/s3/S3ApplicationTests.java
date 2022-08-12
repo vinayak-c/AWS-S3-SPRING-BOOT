3a838a4799f69613c58b85451f8612a998c314bc
Vinayak <vinayakchaurasia99@gmail.com>
2022-08-13 00:02:53 +0530

    Initial commit for S3 poc with spring boot

---
+++ src/test/java/com/vinayak/aws/s3/S3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinayak.aws.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
